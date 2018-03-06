707526746180266ab6544fe8b59be96bee4d9c23
Your Name <you@example.com>
2018-03-05 21:22:52 -0800

    second commit,  still doesnt work but the output is not an error

--- enc.js
+++ enc.js
@@ -6,8 +6,12 @@ var hexToBin = bc(16,2);
 
 function xOR(a,b){
 	var combin = a+b;
+	console.log("combination "+combin)
 	switch (combin) {
-        case (0 || 2):
+        case (0):
+            return 0;
+            break;
+        case (2):
             return 0;
             break;
         case(1):
@@ -20,42 +24,56 @@ var key = process.argv[2];
 
 var message = process.argv[3];
 
-binary_key = hexToBin(key);
+var binary_key = hexToBin(key);
+console.log(binary_key)
 
-binary_message = stb(message);
+var binary_message = stb(message);
+console.log(binary_message);
 
 function longify_key(message, inkey){
-	var key = inkey.toString;
-	var key_len_message;
-	var msg_len = message.length;
+	var key = inkey+'';
+	console.log("key: " + key);
+	var msg_len = message.length; 
 	var key_len = key.length;
-	var tail = msg_len % key_len;
-	var multiples = (msg_len - tail)/key_len;
-	key = tail;
-	for (n in multiples){
-		key += key;
+	var tail_len = msg_len % key_len;
+	var tail = key.split("");
+	var tail_start = key_len-tail_len;
+	tail = tail.splice(tail_start,tail.length);
+	console.log("tail is "+tail);
+	var multiples = (msg_len - tail_len)/key_len;
+	key_len_message = tail;
+	for (var n in multiples){
+		key_len_message += key;
 	};
-	return key;
+	return key_len_message;
 };
 
 
 function xORencrypt(inkey,message){
-	var key = longify_key(inkey);
-	var key_arr = key.split();
-	var message_arr = message.split();
-	var encrypted_msg;
-	for (var n in key_arr.length){
-		var el_1 = key_arr[n];
-		var el_2 = message_arr[n];
+	var key = longify_key(message,inkey);
+	console.log("key_long = " + key);
+	var key_arr = key;
+	console.log("key arr "+ key_arr)
+	var message_arr = message.split('');
+	console.log("message array "+message_arr)
+	var encrypted_msg = '';
+	for (var n in key_arr){
+		var el_1 = parseInt(key_arr[n]);
+		// console.log("el1 "+el_1);
+		var el_2 = parseInt(message_arr[n]);
+		// console.log("el2 "+el_2)
 		var temp = xOR(el_1,el_2);
-		encrypted_msg.prepend(temp);
+		console.log("temp"+temp);
+		encrypted_msg+= temp;
+		// console.log(encrypted_msg)
 	};
+	console.log("encrypted :"+encrypted_msg)
 	return encrypted_msg;
 };
 
 var output = xORencrypt(binary_key,binary_message);
-console.log(output);
-module.exports(output);
+console.log("here: "+output);
+module.exports = output;
 
 
 
