713bf7a37cdc0301956df7dbcfdd8d6faf2761ad
nwactor <nickwactor@gmail.com>
2018-03-06 13:27:29 -0800

    doens't allow the user to go to main unless they're logged in

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,32 +6,33 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    console.log(req.user);
     if (req.user) {
       res.redirect("/main");
     }
     res.sendFile(path.join(__dirname, "../views/login.html"));
   });
 
-  //To trap those users that arent logged in trying to access this page
-  app.get("/main", function(req, res) {
-    res.sendFile(path.join(__dirname,"../views/main.html"));
-    // if(req.user){
-    //   res.redirect("/main");
-    // }
-    // res.sendFile(path.join(__dirname, "../views/login.html"));
-  });
-
   app.get("/signup", function(req, res) {
     res.sendFile(path.join(__dirname, "../views/signup.html"));
   });
 
-  app.get("/logout", function(req,res){
-    res.redirect("/");
+  //To trap those users that arent logged in trying to access this page
+  app.get("/main", function(req, res) {
+    if(req.user){
+      res.sendFile(path.join(__dirname,"../views/main.html"));
+    } else {
+      res.redirect("/");
+    }
   });
 
   // app.get("/main", isAuthenticated, function(req, res){
   //   res.sendFile(path.join(__dirname,"../views/main.html"));
   // });
+  
+  app.get("/logout", function(req,res){
+    res.redirect("/");
+  });
+
+  
 
 };
