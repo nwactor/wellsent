a8c3f49880a0a6d3ae0aa3c123519a42e21c8535
nwactor <nickwactor@gmail.com>
2018-03-07 13:00:42 -0800

    successful key test with messagepool

--- public/assets/js/mainPageScript.js
+++ public/assets/js/mainPageScript.js
@@ -1,13 +1,17 @@
-var user;
-
-//somehow get the user
-//clear user when logout is clicked
-
-// if(user == undefined) {
-// 	window.location.href = "/";
-// }
-
-// $('#logout-btn').on('click', function() {
-// 	window.location.href = "/";
-// });
-
+// This file just does a GET request to figure out which user is logged in
+// and updates the HTML on the page
+$.get("/api/user_data").then(function(data) {
+  $(".username").text(data.username);
+});
+/*
+$('#send-btn').on('click', function() {
+  var message = $('#message-input').val().trim();
+  if(message != '') {
+    var encodedMessage = locksmith(message);
+    
+  }
+});
+*/
+$.post("/api/messagePool", {username: "denis", receivername: "hillary"}).then(function(res) {
+	console.log(res);
+});
