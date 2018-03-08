fed0f1608e1cd05ccf05ffd058c7a2c631b4ee1d
yiyi098 <yiyi098@gmail.com>
2018-03-07 16:07:30 -0800

    fixed main.html

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,15 @@
+DROP DATABASE IF EXISTS wellsentDB;
 CREATE DATABASE wellsentDB;	
 
 USE wellsentDB;
 
-CREATE TABLE users (
-	user_id INTEGER(11) AUTO_INCREMENT NOT NULL,
-	username VARCHAR(30) NOT NULL,
-	pw VARCHAR(30) NOT NULL,
-	email VARCHAR(30) NOT NULL,
-	PRIMARY KEY (user_id)
-);
+-- CREATE TABLE users (
+-- 	user_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+-- 	username VARCHAR(30) NOT NULL,
+-- 	pw VARCHAR(30) NOT NULL,
+-- 	email VARCHAR(30) NOT NULL,
+-- 	PRIMARY KEY (user_id)
+-- );
 
-INSERT INTO users (username, pw, email)
-VALUES  ('mict2000', 'gophercureself', 'anon@anon.com')
+-- INSERT INTO users (username, pw, email)
+-- VALUES  ('mict2000', 'gophercureself', 'anon@anon.com')
